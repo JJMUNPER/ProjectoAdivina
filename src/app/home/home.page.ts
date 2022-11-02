16e614962d9332677ba20ab42b302702f481a58d
JJMUNPER <jmunper007@g.educaand.es>
2022-11-02 12:42:23 +0100

    Finalizacion del video 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  num: number;
+  numSecret: number = this.numAleatorio(1, 100);
+  mayorMenor: string = '...';
+
+  constructor() {
+    console.log("El numero secreto es: " + this.numSecret);
+  }
+  
+  numAleatorio(a , b){
+    return Math.round(Math.random()*(b-a)+ parseInt(a));
+  }
+
+  compruebaNumero(){
+    if (this.num){
+      if(this.numSecret < this.num){
+        this.mayorMenor = 'menor';
+      } else if(this.numSecret > this.num){
+        this.mayorMenor = 'mayor';
+      } else {
+        this.mayorMenor = 'igual';
+      }
+    }
+  }
 
 }
